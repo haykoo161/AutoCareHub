865440cadbce79e426cded23cb7a8e6665c9ff96
haykoo161 <haykoo161@gmail.com>
2024-01-20 21:20:45 +0400

    main page, login, 2nd activity

---
+++ app/src/main/java/com/example/autocarehub/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.autocarehub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button res,forum,about_app;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        res = findViewById(R.id.res);
+        forum = findViewById(R.id.forum);
+        about_app = findViewById(R.id.about_app);
+
+        res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openResources();
+            }
+        });
+
+    }
+    public void openResources() {
+        Intent intent = new Intent(this, Resources.class);
+        startActivity(intent);
+    }
+}
