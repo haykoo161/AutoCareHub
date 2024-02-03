7a231ab70f9edc031e39379b4b5b0563439aa357
haykoo161 <haykoo161@gmail.com>
2024-02-04 00:03:04 +0400

    main page, login, 2nd activity.

--- app/src/main/java/com/example/autocarehub/MainActivity.java
+++ app/src/main/java/com/example/autocarehub/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.autocarehub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button res,forum,about_app;
+import com.google.android.material.tabs.TabLayout;
 
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private ViewPagerAdapter adapter;
+    Button res, forum, about_app;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +28,60 @@ public class MainActivity extends AppCompatActivity {
         forum = findViewById(R.id.forum);
         about_app = findViewById(R.id.about_app);
 
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Register"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
         res.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {openResources();}
+            public void onClick(View v) {
+                openResources();
+            }
         });
         forum.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {openForum();}
+            public void onClick(View v) {
+                openForum();
+            }
         });
 
     }
+
     public void openResources() {
         Intent intent = new Intent(this, Resources.class);
         startActivity(intent);
     }
+
     public void openForum() {
         Intent intent = new Intent(this, register.class);
         startActivity(intent);
