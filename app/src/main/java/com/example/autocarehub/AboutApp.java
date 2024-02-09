883b1e01491907cc923acbc656a139dfec819c69
haykoo161 <haykoo161@gmail.com>
2024-02-09 23:32:08 +0400

    second activities for 3 main buttons

--- app/src/main/java/com/example/autocarehub/AboutApp.java
+++ app/src/main/java/com/example/autocarehub/AboutApp.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class AboutApp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_about_app);
     }
 }
--- app/src/main/java/com/example/autocarehub/MainActivity.java
+++ app/src/main/java/com/example/autocarehub/MainActivity.java
@@ -1,64 +1,41 @@
 package com.example.autocarehub;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.viewpager2.widget.ViewPager2;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.material.tabs.TabLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager2;
-    private ViewPagerAdapter adapter;
+    private Button button;
+
     Button res, forum, about_app;
 
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         res = findViewById(R.id.res);
         forum = findViewById(R.id.forum);
         about_app = findViewById(R.id.about_app);
 
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager2 = findViewById(R.id.view_pager);
-
-        tabLayout.addTab(tabLayout.newTab().setText("Login"));
-        tabLayout.addTab(tabLayout.newTab().setText("Register"));
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        adapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
-        viewPager2.setAdapter(adapter);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        about_app.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager2.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+            public void onClick(View v) {
+                openAboutApp();
             }
         });
 
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+        forum.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageSelected(int position) {
-                tabLayout.selectTab(tabLayout.getTabAt(position));
+            public void onClick(View v) {
+                openForum();
             }
         });
 
@@ -68,22 +45,22 @@ public class MainActivity extends AppCompatActivity {
                 openResources();
             }
         });
-        forum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openForum();
-            }
-        });
 
     }
 
-    public void openResources() {
-        Intent intent = new Intent(this, Resources.class);
+    public void openAboutApp() {
+        Intent intent = new Intent(this, Forum.class);
         startActivity(intent);
     }
 
     public void openForum() {
-        Intent intent = new Intent(this, register.class);
+        Intent intent = new Intent(this, Forum.class);
+        startActivity(intent);
+    }
+
+
+    public void openResources() {
+        Intent intent = new Intent(this, Resources.class);
         startActivity(intent);
     }
 }
